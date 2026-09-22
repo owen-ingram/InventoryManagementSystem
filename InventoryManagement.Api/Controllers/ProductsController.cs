67f0f2dd8fa01cdf880d1939faa3f84368f15f7f
owen <oingramcs@gmail.com>
2026-09-22 01:26:42 -0700

    Implement SQL Server integration and product API endpoints

---
+++ InventoryManagement.Api/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using InventoryManagement.Api.Data;
+using InventoryManagement.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        var products = await _context.Products.ToListAsync();
+
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProductById(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        _context.Products.Add(product);
+
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetProductById),
+            new { id = product.Id },
+            product);
+    }
+}
---
+++ InventoryManagement.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManagement.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
