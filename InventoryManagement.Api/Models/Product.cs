6d76a10a74c3f30865bb45c3da21a1cfe6e238d8
owen <oingramcs@gmail.com>
2026-09-23 01:07:00 -0700

    Add product input validation

--- InventoryManagement.Api/Models/Product.cs
+++ InventoryManagement.Api/Models/Product.cs
@@ -1,14 +1,19 @@
-﻿namespace InventoryManagement.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagement.Api.Models;
 
 public class Product
 {
     public int Id { get; set; }
 
+    [Required]
     public string Name { get; set; } = string.Empty;
 
     public string Description { get; set; } = string.Empty;
 
+    [Range(typeof(decimal), "0.01", "999999.99")]
     public decimal Price { get; set; }
 
+    [Range(0, int.MaxValue)]
     public int Quantity { get; set; }
 }
